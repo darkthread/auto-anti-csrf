55c657ff40a0d8d683295918e48daa7b6065d287
Jeffrey <572199+darkthread@users.noreply.github.com>
2025-11-13 23:24:09 +0800

    Use default RequestVerificationToken header name

--- Program.cs
+++ Program.cs
@@ -1,10 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 
 var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddAntiforgery(options => 
-{
-    options.HeaderName = "X-XSRF-TOKEN"; // Custom header name for AJAX requests
-});
 builder.Services.AddControllersWithViews(options =>
 {
     options.Filters.Add(new AutoValidateAntiforgeryTokenAttribute());
